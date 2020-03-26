2d8f73ab45bcaf64fb72838d9ffa6928ae16458e
Joel Spielberger <joelspielberger101@gmail.com>
2020-03-26 16:59:11 -0700

    added search functionality with useState and useEffect

--- employeetracker/src/components/EmployeeDisplay.js
+++ employeetracker/src/components/EmployeeDisplay.js
@@ -6,19 +6,31 @@ export default function () {
     //hook, set state to empty array because of the data type in this case...
     //if it was an object, initial state would be as well...
     const [employees, setEmployee] = useState([]);
+    const [filteremployees, setFilter] = useState([]);
 
     useEffect(()=>{
     API().then(res=> {
     setEmployee(res.data.results);
+    setFilter(res.data.results);
     console.log(res.data.results)
     })
     },[])
     ;
+
+    const handleChange = val => {
+        console.log(val)
+        let empFilter = employees.filter(a=> a.name.first.toLowerCase().includes(val))
+        setFilter(empFilter);
+    }
     
     return (
             <div className="container">
+                <input onChange={(e)=>{handleChange(e.target.value)}}/>
                 <div className="col-10">
-                    {employees.map(emp=> <div>{emp.name.first}</div>)}
+                    {filteremployees.map(emp=>
+                    <div className="row">Name: {emp.name.title}: {emp.name.first} {emp.name.last} Email: {emp.email}</div>
+                    )
+                    }
                 
                 </div>
             </div>
